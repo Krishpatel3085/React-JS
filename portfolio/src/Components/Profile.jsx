e56df07f74f727b08de2a4cf73e491b792980433
Krishpatel3085 <f388krish@gmail.com>
2024-06-18 21:56:40 +0530

    New Change

--- portfolio/src/Components/Profile.jsx
+++ portfolio/src/Components/Profile.jsx
@@ -30,62 +30,67 @@ export default function Profile() {
                 <div className="row " >
                     <div className="col-md-12 " >
                         <div id="Profile">
+                            <div className="firs">
 
-                            <img src={imge} alt="" />
-                            <div id="name">
-
-                                <h3>Krish Kaila</h3>
-                                <h4>FullStack-Devloper</h4>
+                                <img src={imge} alt="" />
+                                <div id="name">
+                                    <h3>Krish Kaila</h3>
+                                    <h4>FullStack-Devloper</h4>
+                                </div>
+                                <button className='download' onClick={downloadResume}>Download CV</button>
                             </div>
-                            <button className='download' onClick={downloadResume}>Download CV</button>
-
                             <br />
 
-                            <div id='Line'>
-                                <div className="info">
-                                    <div className="icon">
-                                        <i class="bi bi-envelope"></i>
-                                    </div>
-                                    <ul>
-                                        <li id='title'>EMAIL</li>
-                                        <li>f388krish28@gmail.com</li>
-                                    </ul>
-                                </div>
-                                <div className="info">
-                                    <div className="icon">
-                                        <i class="bi bi-phone"></i>
-                                    </div>
-                                    <ul>
-                                        <li id='title'>PHONE</li>
-                                        <li>+98246 99398</li>
-                                    </ul>
-                                </div>
-                                <div className="info">
-                                    <div className="icon">
-                                        <i class="bi bi-calendar3"></i>
-                                    </div>
-                                    <ul>
-                                        <li id='title'>BIRTHDAY</li>
-                                        <li>August 30 ,2005</li>
-                                    </ul>
-                                </div>
-                                <div className="info">
-                                    <div className="icon">
-                                        <i class="bi bi-geo-alt"></i>
-                                    </div>
-                                    <ul>
-                                        <li id='title'>LOCATION</li>
-                                        <li>Ahemdabad , Gujarat</li>
-                                    </ul>
-                                </div>
+                            <label for="check">Show Contact</label>
+                            <input type="checkbox" id="check" />
+                            <ol>
 
-                                <div className="social">
-                                    <a href="https://www.linkedin.com/in/kaila-krish3085/"> <i class="bi bi-linkedin"> </i></a>
-                                    <i class="bi bi-github"></i>
-                                    <i class="bi bi-instagram"></i>
-                                    <br /><br />
+                                <div id='Line'>
+                                    <div className="info">
+                                        <div className="icon">
+                                            <i class="bi bi-envelope"></i>
+                                        </div>
+                                        <ul>
+                                            <li id='title'>EMAIL</li>
+                                            <li>f388krish28@gmail.com</li>
+                                        </ul>
+                                    </div>
+                                    <div className="info">
+                                        <div className="icon">
+                                            <i class="bi bi-phone"></i>
+                                        </div>
+                                        <ul>
+                                            <li id='title'>PHONE</li>
+                                            <li>+98246 99398</li>
+                                        </ul>
+                                    </div>
+                                    <div className="info">
+                                        <div className="icon">
+                                            <i class="bi bi-calendar3"></i>
+                                        </div>
+                                        <ul>
+                                            <li id='title'>BIRTHDAY</li>
+                                            <li>August 30 ,2005</li>
+                                        </ul>
+                                    </div>
+                                    <div className="info">
+                                        <div className="icon">
+                                            <i class="bi bi-geo-alt"></i>
+                                        </div>
+                                        <ul>
+                                            <li id='title'>LOCATION</li>
+                                            <li>Ahemdabad , Gujarat</li>
+                                        </ul>
+                                    </div>
+
+                                    <div className="social">
+                                        <a href="https://www.linkedin.com/in/kaila-krish3085/"> <i class="bi bi-linkedin"> </i></a>
+                                        <i class="bi bi-github"></i>
+                                        <i class="bi bi-instagram"></i>
+                                        <br /><br />
+                                    </div>
                                 </div>
-                            </div>
+                            </ol>
                         </div>
                     </div>
                 </div>
